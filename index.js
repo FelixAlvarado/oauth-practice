28f60acd3339f293e7b5c876a36b1f26d735f59e
FelixAlvarado <felixalvarado653@gmail.com>
2018-10-04 21:59:47 -0700

    setup passport basics

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const express = require('express');
+
+const passport = require('passport');
+const GoogleStretegy = require('passport-google-oauth20').Strategy;
+
+passport.use(new GoogleStretegy());
+
 const app = express();
+
+
 app.listen(3000, () => console.log('listening on 3000'));
 
