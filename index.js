d35ff4743c677739de8b4ffb69497f5dc93d5d27
FelixAlvarado <felixalvarado653@gmail.com>
2018-10-04 22:41:18 -0700

    setup keys and google strategy

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
+//current doing testing oauth
+
 const express = require('express');
 
 const passport = require('passport');
 const GoogleStretegy = require('passport-google-oauth20').Strategy;
+const keys = require('.config/keys');
 
-passport.use(new GoogleStretegy());
+passport.use(
+    new GoogleStretegy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+}, (accessToken) => {
+    console.log(accessToken);
+})
+);
 
 const app = express();
 
 
-app.listen(3000, () => console.log('listening on 3000'));
+app.listen(5000, () => console.log('listening on 5000'));
 
