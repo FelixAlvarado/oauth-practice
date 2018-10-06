4273830e448aa0e718a119c213138fd8d84dbeb7
FelixAlvarado <felixalvarado653@gmail.com>
2018-10-05 18:13:51 -0700

    completed oauth with passport

--- index.js
+++ index.js
@@ -11,16 +11,25 @@ passport.use(
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
-}, (accessToken) => {
-    console.log(accessToken);
+}, (accessToken, refreshToken, profile, done) => {
+    console.log('accessToken',accessToken);
+    console.log('refresh Token', refreshToken);
+    console.log('profile', profile);
+    console.log('done', done);
 })
 );
 
 const app = express();
 
+
+//initial request to get use account info
 app.get('/auth/google', passport.authenticate('google', {
     scope: ['profile', 'email']
 }));
+
+
+//using google access token and get use profile information. accesstoken in google stretegy will have info at this point
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 
 
